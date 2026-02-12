34450664a888fce6715eee41bb3d92c65342891b
JulesEfrei <jules.bruzeau@efrei.net>
2026-02-12 14:43:05 +0100

    feat: generate a deck of 52 cards

---
+++ src/Deck.ts
@@ -0,0 +1,48 @@
+enum Suit {
+  Clubs = 0,
+  Diamonds = 1,
+  Hearts = 2,
+  Spades = 3,
+}
+
+enum Rank {
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Five = 5,
+  Six = 6,
+  Seven = 7,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
+  Jack = 11,
+  Queen = 12,
+  King = 13,
+  Ace = 14,
+}
+
+export type Card = {
+  rank: Rank;
+  suit: Suit;
+};
+
+export type Deck = Card[];
+
+interface DeckGeneratorInterface {
+  generateDeck(): Deck;
+}
+
+export class DeckGenerator implements DeckGeneratorInterface {
+  generateDeck() {
+    let deck: Deck = [];
+
+    for (let suit = 0; suit < 4; suit++) {
+      for (let rank = 2; rank < 15; rank++) {
+        const card: Card = { rank: rank, suit: suit };
+        deck.push(card);
+      }
+    }
+
+    return deck;
+  }
+}
---
+++ tests/Deck.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { DeckGenerator } from "../src/Deck";
+
+const deckGenerator = new DeckGenerator();
+
+describe("Generate a card game", () => {
+  it("should generate a deck of 52 cards", () => {
+    const deck = deckGenerator.generateDeck();
+    expect(deck.length).toBe(52);
+  });
+
+  it("should have equal card number for each suit", () => {
+    const deck = deckGenerator.generateDeck();
+    expect(deck.filter((card) => card.suit === 0).length).toBe(13);
+    expect(deck.filter((card) => card.suit === 1).length).toBe(13);
+    expect(deck.filter((card) => card.suit === 2).length).toBe(13);
+    expect(deck.filter((card) => card.suit === 3).length).toBe(13);
+  });
+});
