6b2599e4f9f3b42811f7c5f7d83d30b6498917bb
moudjane <mathis.oudjane@gmail.com>
2026-02-12 15:43:49 +0100

    feat: ranking of the best hands

---
+++ src/Hand.ts
@@ -0,0 +1,11 @@
+enum HandCategories {
+    BestCard = 0,
+    OnePair = 1,
+    TwoPair = 2,
+    ThreeOfAKind = 3,
+    Straight = 4,
+    Flush = 5,
+    FullHouse = 6,
+    FourOfAKind = 7,
+    StraightFlush = 8,
+}
